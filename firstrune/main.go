0d13a5a9243a311111f08fc3946de94d629aef27
rogwel <anxielworld@gmail.com>
2024-06-23 06:57:47 +0300

    firstrune/

---
+++ firstrune/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		os.Exit(0)
+	}
+	z01.PrintRune(rune(os.Args[1][0]))
+	z01.PrintRune(10)
+}
