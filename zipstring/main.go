997f6932a30c140a3c34dc81346c8e9726911d04
rogwel <anxielworld@gmail.com>
2024-08-22 14:01:31 +0300

    [Docs]: Add documentation comments in the code

--- zipstring/main.go
+++ zipstring/main.go
@@ -8,19 +8,22 @@ import (
 
 func ZipString(s string) string {
 	var str string
-	n := len(s)
+	n := len(s) // stores the length of the string s...
 
+	// if no argment is passed, the program returns an empty string...
 	if n == 0 {
 		return ""
 	}
+
+	// initialise count to 1...
 	count := 1
-	for i := 1; i < n; i++ {
+	for i := 1; i < n; i++ { // set an index that runs from the index 1 to the last index...
 		if s[i] == s[i-1] {
 			count++
-		} else {
+		} else { // if the prefixing character is not equal to the previous one, count is retained as 1.
 			str += (strconv.Itoa(count))
 			str += string(s[i-1])
-			count = 1
+			count = 1 // the value of the count is reset back to 1... and the loop moves to the next i value...
 		}
 	}
 	str += strconv.Itoa(count)
